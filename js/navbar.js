2869683c7f8c90281814d0091666d521a6ff8c25
Amexon <jakubsoj13@gmail.com>
2026-08-06 12:18:43 +0200

    update

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+fetch("navbar.html")
+.then(res => res.text())
+.then(data => {
+    document.getElementById("navbar").innerHTML = data;
+
+    let page = location.pathname.split("/").pop();
+
+    document.querySelectorAll(".navbar a").forEach(a => {
+        if (a.href.includes(page)) {
+            a.classList.add("active");
+        }
+    });
+});
